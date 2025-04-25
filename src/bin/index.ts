99f9dff42c5d8b95feca128c849e71491bd04502
Julio Hurtado <juliens@outlook.com>
2025-04-24 19:41:03 -0500

    chore: add security settings

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,21 +1,30 @@
 import { expressApp, port, routes } from '#inject';
 import { getBaseContextMiddleware, requestContextMiddleware } from '#middlewares/contextMiddleware';
 
+import { apiLimiter } from '#middlewares/rateLimiter';
 import config from '#config';
 import connectDB from '#config/database';
 import cors from 'cors';
 import { errorHandler } from '#middlewares/errorHandler';
 import express from 'express';
+import mongoSanitize from 'express-mongo-sanitize';
 import openApi from '#config/swagger';
 import registerRoutes from '#routes/index.route';
 import swaggerUi from 'swagger-ui-express';
+import xss from 'xss-clean';
 
 export async function bootstrap(): Promise<void> {
   expressApp.disable('x-powered-by');
-  expressApp.use(cors());
+
+  expressApp.use(mongoSanitize());
+  expressApp.use(xss());
+  expressApp.use('/api', apiLimiter);
+
   expressApp.use(express.json());
+  expressApp.use(cors());
   expressApp.use(getBaseContextMiddleware);
   expressApp.use(requestContextMiddleware);
+
   registerRoutes(expressApp, routes, '/api');
 
   await connectDB(config.db.url);
